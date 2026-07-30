af99b266de7be204f4d5234618f166451195ec99
Asmi-bot-tech <asmi.thapa.a24@icp.edu.np>
2026-07-30 10:44:23 +0545

    my app

---
+++ VITE/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
---
+++ VITE/my-app/src/components/Navbar.jsx
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <h1>This is NavBar</h1>;
+}
