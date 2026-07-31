eac4f21afa1ffad2177ab404544e8e618f7d5c23
Asmi-bot-tech <asmi.thapa.a24@icp.edu.np>
2026-07-31 10:57:43 +0545

    i changed the colour from bg blue 600 to 800 navy blue

--- VITE/my-app/src/components/Footer.jsx
+++ VITE/my-app/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="bg-blue-600 text-white text-center p-4 mt-10">
+      <footer className="bg-blue-800 text-white text-center p-4 mt-10">
         <p>2026 My App. All Rights Reserved.</p>
       </footer>
     </>
--- VITE/my-app/src/components/Navbar.jsx
+++ VITE/my-app/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <>
-      <nav className="bg-blue-600 text-white p-4 shadow-md">
+      <nav className="bg-blue-800 text-white p-4 shadow-md">
         <h1 className="text-2x1 font-bold inline">My App</h1>
         <ul className="float-right flex gap-6">
           <li className="hover:text-gray-200 cursor-pointer">Home</li>
