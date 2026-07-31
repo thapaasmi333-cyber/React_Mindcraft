2bcb2147bdfc0b64bff17097380ad4cd60306563
Asmi-bot-tech <asmi.thapa.a24@icp.edu.np>
2026-07-31 10:46:48 +0545

    my home contact and about was sticking together so i did gap-6 i missed the - at first

--- VITE/my-app/src/App.jsx
+++ VITE/my-app/src/App.jsx
@@ -4,9 +4,16 @@ import Navbar from "./components/navbar";
 export default function App() {
   return (
     <>
-      <Navbar />
-      <h2 className="text-3x1 text-center mt-10">Welcome to My YT Channel</h2>
-      <Footer />
+      <main className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <search className="flex-1 flex items-center justify-center">
+          <h2 className="text-3x1 text-center mt-10">
+            Welcome to My YT Channel
+          </h2>
+        </search>
+        <Footer />
+      </main>
     </>
   );
 }
--- VITE/my-app/src/components/Navbar.jsx
+++ VITE/my-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <>
       <nav className="bg-blue-600 text-white p-4 shadow-md">
         <h1 className="text-2x1 font-bold inline">My App</h1>
-        <ul className="float-right flex gap 6">
+        <ul className="float-right flex gap-6">
           <li className="hover:text-gray-200 cursor-pointer">Home</li>
           <li className="hover:text-gray-200 cursor-pointer">About</li>
           <li className="hover:text-gray-200 cursor-pointer">Contact</li>
