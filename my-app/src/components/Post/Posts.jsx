aa2676382d243be34a079647921bd540418069e5
Asmi-bot-tech <asmi.thapa.a24@icp.edu.np>
2026-08-12 13:57:02 +0545

    did the todo homework using the same async and await, did with useState

--- my-app/src/components/Post/Posts.jsx
+++ my-app/src/components/Post/Posts.jsx
@@ -1,42 +1,79 @@
 import { useEffect, useState } from "react";
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
+// export default function Posts() {
+//   const [posts, setPosts] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const loadPosts = async () => {
+//       try {
+//         const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+//         if (!res.ok) {
+//           throw new Error("Server error");
+//         }
+//         const data = await res.json();
+//         setPosts(data);
+//       } catch (err) {
+//         setError(err.message);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     loadPosts();
+//   }, []);
+
+//   if (loading) return <h2>Loading..</h2>;
+//   if (error) return <h2>Error: </h2>;
+
+//   return (
+//     <>
+//       <h1> POSTS </h1>
+
+//       {posts.map((post) => (
+//         <div key={post.userId}>
+//           <p>User ID : {post.userId}</p>
+//           <h2>Tittle : {post.title}</h2>
+//           <p> Post : {post.body}</p>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
+export default function Post() {
+  const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const loadPosts = async () => {
+    const loadTodos = async () => {
       try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-
-        if (!res.ok) {
-          throw new Error("Server error");
-        }
+        const res = await fetch(
+          "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        );
         const data = await res.json();
-        setPosts(data);
-      } catch (err) {
-        setError(err.message);
+        setTodos(data);
+      } catch (error) {
+        console.log(error);
       } finally {
         setLoading(false);
       }
     };
-    loadPosts();
+    loadTodos();
   }, []);
 
-  if (loading) return <h2>Loading..</h2>;
-  if (error) return <h2>Error: </h2>;
-
+  if (loading) {
+    return <h2>Loading ... </h2>;
+  }
   return (
     <>
-      <h1> POSTS </h1>
+      <h1>TODO LIST</h1>
 
-      {posts.map((post) => (
-        <div key={post.userId}>
-          <p>User ID : {post.userId}</p>
-          <h2>Tittle : {post.title}</h2>
-          <p> Post : {post.body}</p>
-        </div>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          {todo.completed ? "✅" : "❌"}
+          {todo.title}
+        </p>
       ))}
     </>
   );
